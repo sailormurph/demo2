9572664a9a8aa9e0ab33a214b232b5d147ce9541
andrew <andrew.murphy@ibm.com>
2022-12-27 15:48:38 -0600

    update

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.demo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Demo1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Demo1Application.class, args);
 	}
 
 }
